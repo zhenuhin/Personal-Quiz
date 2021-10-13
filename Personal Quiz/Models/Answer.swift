3d84851f57eae8d0129a891fe4081f946662669e
Alexandr <zhenuhin@inbox.ru>
2021-10-13 11:30:00 +0300

    interface is ready

---
+++ Personal Quiz/Models/Answer.swift
@@ -0,0 +1,8 @@
+//
+//  Answer.swift
+//  Personal Quiz
+//
+//  Created by Александр Женухин on 13.10.2021.
+//
+
+import Foundation
---
+++ Personal Quiz/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  Personal Quiz
+//
+//  Created by Александр Женухин on 13.10.2021.
+//
+
+import Foundation
