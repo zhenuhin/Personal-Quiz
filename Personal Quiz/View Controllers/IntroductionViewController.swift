5e837cb37e4dc6cab8df1d2762f6eb918eaf6be3
Alexandr <zhenuhin@inbox.ru>
2021-10-13 20:33:34 +0300

    Add introductionVC

---
+++ Personal Quiz/View Controllers/IntroductionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  IntroductionViewController.swift
+//  Personal Quiz
+//
+//  Created by Александр Женухин on 13.10.2021.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+    
+}
